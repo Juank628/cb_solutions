46011177a79863dbdc2df156d6fdc57a9586c615
Juank628 <jsanchez_zunino@yahoo.com>
2023-05-19 18:43:33 -0500

    Create about section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -12,6 +12,31 @@ export default function Home() {
   return (
     <main className={styles.container}>
       <Header />
+
+      <h2 className={styles.sectionTitle}>CBsolutions</h2>
+      <hr className={styles.titleLine} />
+      <div className={styles.aboutContainer}>
+        <p>
+          Bienvenido a nuestra empresa de servicios de automatización industrial.
+          Ofrecemos soluciones inteligentes y eficientes para optimizar procesos
+          y aumentar la productividad. Nuestro equipo experto diseña,
+          implementa y mantiene sistemas de control automatizados,
+          adaptados a sus necesidades.
+          Trabajamos con tecnología de vanguardia y proveedores líderes de la industria
+          para ofrecer soluciones innovadoras y confiables. Además de la implementación,
+          brindamos soporte continuo, incluyendo mantenimiento preventivo, diagnóstico y
+          reparación, y actualizaciones tecnológicas.
+        </p>
+        <p className={styles.aboutForLargeScreens}>
+          Nuestro enfoque se centra en comprender sus requerimientos y objetivos para
+          desarrollar soluciones personalizadas que optimicen la eficiencia, reduzcan
+          costos y aumenten la calidad. Si busca mejorar la eficiencia y productividad
+          de su operación industrial, estamos aquí para ayudarlo. Contáctenos hoy mismo
+          y descubra cómo nuestra empresa de automatización industrial puede llevar su
+          negocio al siguiente nivel.
+        </p>
+      </div>
+
       <h2 className={styles.sectionTitle}>Servicios</h2>
       <hr className={styles.titleLine} />
       <div className={styles.servicesContainer}>
